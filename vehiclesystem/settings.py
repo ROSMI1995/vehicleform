492f25c251110aff19969afdb53965524f9dfb71
Rosmi Thomas <96420118+ROSMI1995@users.noreply.github.com>
2022-10-25 15:12:38 +0530

    Update settings.py

--- vehiclesystem/settings.py
+++ vehiclesystem/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +129,4 @@ LOGOUT_REDIRECT_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
